d9371d1e9440010546f9549bc0f632f0c9bfaae0
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-08 22:08:59 +0000

    tidying up

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,13 @@
 import "./Home.css";
 
 export function Home() {
+
   return (
     <div className="home-page flex">
+      <div className="background-animation" id="background-animation">
+        <canvas id="canvas"></canvas>
+        <script src="./js/bgAnimate.js"></script>
+      </div>
       <div className="home-title">Welcome!</div>
     </div>
   );
