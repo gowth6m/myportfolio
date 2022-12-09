b25f2ff3ef608de23ce15e09ba85ec480f216429
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-09 00:21:27 +0000

    updated navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,18 @@
 import "./Home.css";
 
 export function Home() {
-
   return (
     <div className="home-page flex">
-      <div className="background-animation" id="background-animation">
-        <canvas id="canvas"></canvas>
-        <script src="./js/bgAnimate.js"></script>
+      <div className="intro-info">
+        <div className="title plax">
+          I'm <span>Gowtham</span>
+          <span className="title-span">.</span>
+        </div>
+        <div className="title-text plax">
+          A programmer who <span className="typed-text"> </span>
+          <span className="typed-text-cursor">|</span>
+        </div>
       </div>
-      <div className="home-title">Welcome!</div>
     </div>
   );
 }
