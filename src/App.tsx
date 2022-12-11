b3bbcc11e3941428debfc32ac3a7239794212976
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-11 18:59:20 +0000

    updated some files and added footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/nav/NavBar";
 import { Home } from "./components/home/Home";
 import { Portfolio } from "./components/portfolio/Portfolio";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
+      <Footer />
     </>
   );
 }
