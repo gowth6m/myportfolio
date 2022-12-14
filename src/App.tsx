f1f9f9690e3f75c39757c542cf3d907e9131db9e
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-14 23:20:08 +0000

    added auto scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,22 @@ import { Home } from "./components/home/Home";
 import { Portfolio } from "./components/portfolio/Portfolio";
 import { Footer } from "./components/footer/Footer";
 import { PageNotFound } from "./components/misc/PageNotFound";
+import ScrollToTop from "./components/misc/ScrollToTop";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </>
   );
 }
 
 export default App;
-
---
+++ src/components/misc/ScrollToTop.tsx
@@ -0,0 +1,21 @@
+// ScrollToTop.jsx
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const handleClickScroll = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+const ScrollToTop = (props:any) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{props.children}</>
+};
+
+export default ScrollToTop;
