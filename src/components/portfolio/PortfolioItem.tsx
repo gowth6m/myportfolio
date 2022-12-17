8e70a4d379472b326a9949dfccd555f9d57a38ce
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-17 00:30:08 +0000

    changes to portfolio

--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -1,134 +1,111 @@
 import { motion, Variants } from "framer-motion";
+import { useState } from "react";
 import "./PortfolioItem.css";
 
-class PortfolioItem {
-  key: number;
+export type PortfolioItemProps = {
+  id: number;
   title: string;
   description: string;
   imageUrl: string;
   link: string;
   github: string;
   stack: string[];
-  filters?: string[];
+  filters: string[];
+};
 
-  constructor(
-    key: number,
-    title: string,
-    description: string,
-    imageUrl: string,
-    link: string,
-    github: string,
-    stack: string[],
-    filters?: string[]
-  ) {
-    this.key = key;
-    this.title = title;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this.link = link;
-    this.github = github;
-    this.stack = stack;
-    this.filters = ["all", ...filters!]; // forcing it to have 'all' filter
-  }
+export default function PortfolioItem({
+  id,
+  title,
+  description,
+  imageUrl,
+  link,
+  github,
+  stack,
+  filters,
+}: PortfolioItemProps) {
+  const [showImage, setShowImage] = useState(false);
 
-  openSocial = (socialLink: string) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
+  const openSocial = (socialLink: string) => {
     window.open(socialLink);
   };
 
-  showImage: boolean = false;
+  return (
+    <motion.div
+      key={id}
+      className="portfolio-item-container"
+      transition={{
+        duration: 1,
+        delay: 0,
+      }}
+      viewport={{ once: true }}
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={itemVariants}
+    >
+      <div className="portfolio-item-header">
+        <i
+          className="portfolio-item-header-item fa-regular fa-3x fa-folder"
+          onClick={() => {
+            setShowImage(!showImage);
+          }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        ></i>
 
-  handleMouseOver = () => {
-    this.showImage = true;
-    console.log('mouse over');
-  };
-
-  handleMouseOut = () => {
-    this.showImage = false;
-    console.log('mouse out');
-  };
-
-  render() {
-    return (
-      <motion.div
-        key={this.key}
-        className="portfolio-item-container"
-        transition={{
-          duration: 1,
-          delay: 0,
-        }}
-        viewport={{ once: true }}
-        initial="offscreen"
-        whileInView="onscreen"
-        variants={this.key % 2 === 1 ? itemVariants : itemVariants}
-      >
-        <div className="portfolio-item-header">
-          <i
-            className="portfolio-item-header-item fa-regular fa-3x fa-folder"
-            onMouseOver={this.handleMouseOver}
-            onMouseOut={this.handleMouseOut}
-          ></i>
-          <div className="portfolio-item-header-links">
-            {this.github !== "" ? (
-              <i
-                className="fa-brands fa-xl fa-github"
-                onClick={() => {
-                  this.openSocial(this.github);
-                }}
-              ></i>
-            ) : null}
-            {this.link !== "" ? (
-              <i
-                className="fa-solid fa-xl fa-arrow-up-right-from-square"
-                onClick={() => {
-                  this.openSocial(this.link);
-                }}
-              ></i>
-            ) : null}
-          </div>
+        <div className="portfolio-item-header-links">
+          {github !== "" ? (
+            <i
+              className="fa-brands fa-xl fa-github"
+              onClick={() => {
+                openSocial(github);
+              }}
+            ></i>
+          ) : null}
+          {link !== "" ? (
+            <i
+              className="fa-solid fa-xl fa-arrow-up-right-from-square"
+              onClick={() => {
+                openSocial(link);
+              }}
+            ></i>
+          ) : null}
         </div>
+      </div>
 
-        <div className="portfolio-item-body">
-          <div className="portfolio-item-body-title">{this.title}</div>
-          <div className="portfolio-item-body-desc">{this.description}</div>
-          <div className="portfolio-item-body-tag-container">
-            {this.stack?.map((e) => {
-              return (
-                <div key={e} className="portfolio-item-body-tag">
-                  {e}
-                </div>
-              );
-            })}
-          </div>
+      <div className="portfolio-item-body">
+        <div className="portfolio-item-body-title">{title}</div>
+        <div className="portfolio-item-body-desc">{description}</div>
+        <div className="portfolio-item-body-tag-container">
+          {stack?.map((e) => {
+            return (
+              <div key={e} className="portfolio-item-body-tag">
+                {e}
+              </div>
+            );
+          })}
         </div>
+      </div>
 
-        {/* {this.showImage ? <img
+      {isHovering ? (
+        <img
           loading="lazy"
           className="portfolio-item-img"
           alt="portfolioItemImg"
-          src={
-            process.env.PUBLIC_URL + "/assets/portfolio_assets/" + this.imageUrl
-          }
-        ></img> : null} */}
-
-        {/* <img
-          loading="lazy"
-          className="portfolio-item-img"
-          alt="portfolioItemImg"
-          src={
-            process.env.PUBLIC_URL + "/assets/portfolio_assets/" + this.imageUrl
-          }
-        ></img> */}
-
-        {/* <div className="portfolio-item-details flex">
-          <div className="portfolio-item-title">{this.title}</div>
-        </div> */}
-
-        {/* <div className="portfolio-item-bot">
-          <div></div>
-        </div> */}
-      </motion.div>
-    );
-  }
+          src={process.env.PUBLIC_URL + "/assets/portfolio_assets/" + imageUrl}
+        ></img>
+      ) : null}
+    </motion.div>
+  );
 }
 
 const itemVariants: Variants = {
@@ -144,5 +121,3 @@ const itemVariants: Variants = {
     },
   },
 };
-
-export default PortfolioItem;
